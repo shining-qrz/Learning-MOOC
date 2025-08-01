1b41cff9677e87a66468b7e72e1ddb77e84d6547
shining <1909906595@qq.com>
2025-08-01 11:47:01 +0800

    feat: 内容管理模块初始化

---
+++ services/content/src/main/java/edu/wust/qrz/ContentMainApplication.java
@@ -0,0 +1,11 @@
+package edu.wust.qrz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContentMainApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ContentMainApplication.class);
+    }
+}
