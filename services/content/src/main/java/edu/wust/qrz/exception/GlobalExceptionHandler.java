6cc8c003f72d62c2babb840fe02b2ca9299f2c2e
shining <1909906595@qq.com>
2025-08-03 14:45:04 +0800

    feat: 记录异常处理信息

--- services/content/src/main/java/edu/wust/qrz/exception/GlobalExceptionHandler.java
+++ services/content/src/main/java/edu/wust/qrz/exception/GlobalExceptionHandler.java
@@ -1,9 +1,11 @@
 package edu.wust.qrz.exception;
 
 import edu.wust.qrz.common.Result;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
@@ -14,6 +16,7 @@ public class GlobalExceptionHandler {
      */
     @ExceptionHandler(Exception.class)
     public Result handleException(Exception e) {
+        log.error("顶层异常抛出: {}", e.getMessage());
         return Result.fail("服务器异常: " + e.getMessage());
     }
 }
